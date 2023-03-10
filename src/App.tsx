48457704acd73af9f4fe906bc4a38bc03fd12de4
Atanas <atakan08.k@gmail.com>
2023-03-10 10:41:45 +0200

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import { generate } from './generator';
 
 function App() {
   return (
+
+
     <div className="App">
-     {
-      generate(3,"{126")
-     }
+      <div className="container">
+        <label className="input">
+          <b>Enter your password: </b>
+          <input></input>
+          <b className="numberinput">Offset: </b>
+          <input type="number" min={1}></input>
+          <p className="checkbox">option1</p>
+          <input type="checkbox"></input>
+          <span className="checkmark"></span>
+        </label>
+        <button className="button">Enter</button>
+        {generate(5, "AZsumAZ")}
+      </div>
     </div>
+
+
   );
 }
-
 export default App;
+
