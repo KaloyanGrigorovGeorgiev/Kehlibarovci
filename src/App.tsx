4e9ae09497fc065f968c46c0ebdf9d26213a9e7b
Atanas <atakan08.k@gmail.com>
2023-03-10 11:22:40 +0200

    hooked up generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { generate } from './generator';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [offset, setOffset] = useState(1);
   return (
-
-
     <div className="App">
       <div className="container">
         <label className="input">
           <b>Enter your password: </b>
-          <input></input>
+          <input value={password} onChange={event => setPassword(event.target.value)}></input>
           <b className="numberinput">Offset: </b>
-          <input type="number" min={1}></input>
+          <input type="number" min={1} value={offset} onChange={event => setOffset(Number(event.target.value))}></input>
           <p className="checkbox">option1</p>
           <input type="checkbox"></input>
           <span className="checkmark"></span>
         </label>
         <button className="button">Enter</button>
-        {generate(5, "AZsumAZ")}
+        {generate(offset, password)}
+        <b className="rezultat">Result: </b>
+        <input></input>
       </div>
     </div>
-
-
   );
 }
 export default App;
-
