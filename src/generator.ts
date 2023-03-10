80807683d609fe1c6b1c05ecbc757584d6901bfc
Tsetsy <georgievkaloyan19@gmail.com>
2023-03-10 13:38:38 +0200

    added salt to algorithm

--- src/generator.ts
+++ src/generator.ts
@@ -1,9 +1,19 @@
 import './App.css';
-export function generate(n:number,password:string) {
-    function encode(message:string){
+export function generate(n: number, password: string) {
+    function generateSalt(length: number) {
+        let salt = "";
+        do {
+            salt += String.fromCharCode(Math.ceil(Math.random() * 127 + 1));
+        } while (salt.length <= length)
+        return salt;
+    }
+    function encode(message: string) {
         let result = "";
-        for (let i = 0; i < message.length; i++){
-            const shifted = (message[i].charCodeAt(0) + n) % 126;
+        const salt1: string = generateSalt(10);
+        const salt2: string = generateSalt(10);
+        const newPass = salt1 + message + salt2;
+        for (let i = 0; i < newPass.length; i++) {
+            const shifted = (newPass[i].charCodeAt(0) + n) % 126;
             const newSymbol = String.fromCharCode(shifted);
             result += newSymbol;
         }
